5d2dc8b69da2e9c4ba2eec65d6198ac311819ebb
Fernando Mendoza <fern.mendoza13@gmail.com>
2019-01-27 22:47:59 -0800

    got rid of react readme

--- clicky-game/src/components/Nav/Nav.js
+++ clicky-game/src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ const Nav = props => (
   <nav>
     <ul>
       <li className="brand animated lightSpeedIn">
-        <a href="/">{props.title}</a>
+        <a href="/clicky-game">{props.title}</a>
       </li>
 
       <li id="rw">{props.rightwrong}</li>
