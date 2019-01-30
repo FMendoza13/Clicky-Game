46c6293e9e415d72d905b258b8e3f8151af4c197
Fernando Mendoza <fern.mendoza13@gmail.com>
2019-01-30 11:03:03 -0800

    Updates

---
+++ precache-manifest.d05b424e37d9b7ad165a51cb4e03611d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "3377b4f1c40c7669f0d4",
+    "url": "/Clicky-Game/static/js/main.3377b4f1.chunk.js"
+  },
+  {
+    "revision": "040edfbe9947ba1664ee",
+    "url": "/Clicky-Game/static/js/1.040edfbe.chunk.js"
+  },
+  {
+    "revision": "3377b4f1c40c7669f0d4",
+    "url": "/Clicky-Game/static/css/main.a5dd4668.chunk.css"
+  },
+  {
+    "revision": "6de855216a48de2d6fcad3a4afd2ffd7",
+    "url": "/Clicky-Game/index.html"
+  }
+];
