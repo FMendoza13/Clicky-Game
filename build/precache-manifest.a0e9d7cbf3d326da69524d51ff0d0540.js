c0d9d04c1c61696a04a4eb59c78f2e30f77b6442
Fernando Mendoza <fern.mendoza13@gmail.com>
2019-01-30 10:46:26 -0800

    restructured folders

---
+++ build/precache-manifest.a0e9d7cbf3d326da69524d51ff0d0540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3073dd27182f2f05647a",
+    "url": "/static/js/main.3073dd27.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "3073dd27182f2f05647a",
+    "url": "/static/css/main.25ef7db9.chunk.css"
+  },
+  {
+    "revision": "1f90daf176e0f4c7a88323c62a333c9f",
+    "url": "/index.html"
+  }
+];
