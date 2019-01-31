8a8b04021cd01c8c23db0909305d20f753b501df
Fernando Mendoza <fern.mendoza13@gmail.com>
2019-01-30 17:56:47 -0800

    deleted react instruction text

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0e9d7cbf3d326da69524d51ff0d0540.js"
+  "/Clicky-Game/precache-manifest.d05b424e37d9b7ad165a51cb4e03611d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Clicky-Game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
